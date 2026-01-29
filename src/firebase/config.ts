1f96ee6466bbb8959014562243176381b588b472
Dark Hax <theultimateminecraftgamer01@gmail.com>
2026-01-29 19:48:31 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-4864628710-c1e9b",
+  "appId": "1:1011600957053:web:945e8f3044bc2472129b14",
+  "apiKey": "AIzaSyBasiXEaGT6BgJmd2QDnR9sk6OV15Aw-sU",
+  "authDomain": "studio-4864628710-c1e9b.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1011600957053"
+};
